dd40f379dae60162141d95f301c9c083d43c8187
Phil Wittig <pdwittig@gmail.com>
2014-09-08 15:26:23 -0700

    include agency in setting json

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -2,7 +2,7 @@ class Api::SettingsController < ApplicationController
   
   def index
     @settings = AuthToken.find_by_access_token(params[:auth_token]).user.setting
-    render json: @settings
+    render json: @settings.to_json(include: :agency)
   end
 
 end
