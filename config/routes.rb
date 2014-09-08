45a703b9e13611ed3daff991006141aa30a2f1b5
Phil Wittig <pdwittig@gmail.com>
2014-09-08 16:03:38 -0700

    change settings routes to custom because they dont send :id in url - instead they send custom params

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rungo::Application.routes.draw do
   namespace :api do
     resources :users, only: [:create]
-    resources :settings, only: [:create, :index]
     resources :sessions, only: [:create, :destroy]
+    resources :agencies, only: [:index]
+
     resources :non_directional_routes, controller: 'routes', 
       type: 'NonDirectionalRoutes', only: [:index]
-    resources :agencies, only: [:index]
+
+    get 'settings/', to: 'settings#show'
+    put 'settings/', to: 'settings#update'
   end
 end
