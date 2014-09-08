dbb92b674a08d5fe650c941d8df709eee63b2ede
Phil Wittig <pdwittig@gmail.com>
2014-09-08 16:01:20 -0700

    add update method

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -1,7 +1,14 @@
 class Api::SettingsController < ApplicationController
   
-  def index
+  def show
     @settings = AuthToken.find_by_access_token(params[:auth_token]).user.setting
+    render json: @settings.to_json(include: :agency)
+  end
+
+  def update
+    @agency = Agency.find_by_name(params[:agency_name])
+    @settings = AuthToken.find_by_access_token(params[:auth_token]).user.setting
+    @settings.update_attributes agency: @agency
     render json: @settings.to_json(include: :agency)
   end
 
