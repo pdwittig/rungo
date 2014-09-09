12bdc034c3a1022e63accf8857921cb83e6d489f
Phil Wittig <pdwittig@gmail.com>
2014-09-08 23:49:34 -0700

    finish settings controller - for now

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -1,15 +1,21 @@
 class Api::SettingsController < ApplicationController
   
   def show
+    p params
     @settings = AuthToken.find_by_access_token(params[:auth_token]).user.setting
-    render json: @settings.to_json(include: :agency)
+    p @settings.to_json(include: [:agency, :non_directional_route])
+    render json: @settings.to_json(include: [:agency, :non_directional_route, :directional_route])
   end
 
   def update
-    @agency = Agency.find_by_name(params[:agency_name])
+    agency = Agency.find_by_name(params[:agency_name])
+    non_directional_route = NonDirectionalRoute.find_by_name(params[:non_directional_route_name])
+    directional_route = DirectionalRoute.find_by_name(params[:directional_route_name])
     @settings = AuthToken.find_by_access_token(params[:auth_token]).user.setting
-    @settings.update_attributes agency: @agency
-    render json: @settings.to_json(include: :agency)
+    @settings.update_attributes(agency: agency,
+      non_directional_route: non_directional_route,
+      directional_route: directional_route) 
+    render json: @settings.to_json(include: [:agency, :non_directional_route])
   end
 
 end
