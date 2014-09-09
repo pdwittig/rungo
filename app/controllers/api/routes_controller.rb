62837b47ac679877c019dbea894476638399d836
Phil Wittig <pdwittig@gmail.com>
2014-09-08 18:41:58 -0700

    add nondir/dir route lookup in controller

--- app/controllers/api/routes_controller.rb
+++ app/controllers/api/routes_controller.rb
@@ -2,10 +2,25 @@ class Api::RoutesController < ApplicationController
   before_action :set_route_type
 
   def index
-    render json: route_class.all.to_json
+    @routes = get_routes
+    render json: @routes.to_json
   end
 
   private
+
+  def get_routes
+    p route_type
+    p params
+
+    if params[:type] == "NonDirectionalRoutes"
+      p "I am here"
+      agency_id = Agency.find_by_name(params[:agency_name])
+      route_class.where(agency_id: agency_id)
+    elsif params[:type] == "DirectionalRoutes"
+      route_id = Route.name(params[:non_directional_route_name])
+      route_class.where(route_id: route_id)
+    end
+  end
 
   def set_route_type
     @route_type = route_type
